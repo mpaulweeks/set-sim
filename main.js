3dee749874c89a4a9cd2154533e8c1cfe2b8ae23
mpaulweeks <mpaulweeks@gmail.com>
2018-11-25 16:41:45 -0500

    put output in table

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
+function displayMaster(master){
+  const actual = {...master};
+  const percentages = {};
+  Object.keys(actual).forEach(key => {
+    const value = actual[key];
+    const percentKey = key;
+    const percentValue = getPercent(value, actual.total);
+    percentages[percentKey] = percentValue;
+  });
+  delete percentages['ended-06-with-triple-set'];
+  actual['06-with-triple-set'] = actual['ended-06-with-triple-set'];
+  percentages['06-with-triple-set'] = getPercent(actual['ended-06-with-triple-set'], actual['ended-with-06']);
+
+  const keys = Object.keys(percentages).sort();
+  document.getElementById('output').innerHTML = keys.map(key => `
+    <tr>
+      <td>${key}</td>
+      <td>${percentages[key] || ''}</td>
+      <td>${actual[key] || ''}</td>
+    </tr>
+  `).join('');
+}
+
 async function main() {
   let sim = new Simulator();
   let master = {};
-  const percentages = {};
-  const domRaw = document.getElementById('output')
-  const domPercent = document.getElementById('percent')
 
   while (true){
     const stats = await runAsync(() => sim.simulateMany());
     master = mergeObjs(master, stats);
-
-    Object.keys(master).forEach(key => {
-      if (key.startsWith('ended-with-') || key.startsWith('first-sets-')){
-        const value = master[key];
-        const percentKey = key;
-        const percentValue = getPercent(value, master.total);
-        percentages[percentKey] = percentValue;
-      }
-    });
-    percentages['06-with-triple-set'] = getPercent(master['ended-06-with-triple-set'], master['ended-with-06']);
-
-    domRaw.innerHTML = JSON.stringify(master, Object.keys(master).sort(), 2);
-    domPercent.innerHTML = JSON.stringify(percentages, Object.keys(percentages).sort(), 2);
+    displayMaster(master);
   }
 }
 
